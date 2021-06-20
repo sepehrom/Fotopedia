6804efb767836ec3c8cf7b0f0b71e319827c0651
Amir Sepehrom <amir.sepehrom@gmail.com>
2021-06-20 18:14:19 +0430

    Handled API error types

--- Fotopedia/Image_Loader/Remote_Provider/MoyaImageNetworkManager.swift
+++ Fotopedia/Image_Loader/Remote_Provider/MoyaImageNetworkManager.swift
@@ -9,6 +9,17 @@
 import Foundation
 import Moya
 
+enum APIError: LocalizedError {
+	case serverError(message: String)
+	
+	var errorDescription: String? {
+		switch self {
+		case let .serverError(message):
+			return message
+		}
+	}
+}
+
 class MoyaImageNetworkManager: ImageNetworkManager {
 	private var imageSearchResponseParser: ImageSearchResponseParser?
 	private init() {}
@@ -37,8 +48,12 @@ class MoyaImageNetworkManager: ImageNetworkManager {
 					let statusCode = response.statusCode
 					if statusCode == 200 {
 						let responseObject = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String : Any]
-						let imageDataArray = self.imageSearchResponseParser!.parse(responseObject: responseObject!)
-						successCallback(imageDataArray)
+						if (responseObject?["stat"] as! String == "ok") {
+							let imageDataArray = self.imageSearchResponseParser!.parse(responseObject: responseObject!)
+							successCallback(imageDataArray)
+						} else {
+							failureCallback(APIError.serverError(message: responseObject!["message"]! as! String))
+						}
 					}
 				case let .failure(error):
 					failureCallback(error)
